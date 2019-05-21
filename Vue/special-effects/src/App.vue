d4df5bc095870f64d0dddf83b3151fc72b607778
小幸运 <3480782290@qq.com>
2019-05-21 19:13:50 +0800

    ‘'特效’

---
+++ Vue/special-effects/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <cherryBlossoms/>
+  </div>
+</template>
+
+<script>
+import cherryBlossoms from './components/cherryBlossoms.vue'
+export default {
+  name: 'app',
+  components: {
+    cherryBlossoms
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  @import  './assets/css/base.css';
+  #app{
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
